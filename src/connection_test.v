6a5f8e13f312a86a5cad18e691b934619f9ea47e
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-02-25 18:49:54 +0100

    Add database to test_url

--- src/connection_test.v
+++ src/connection_test.v
@@ -1,8 +1,8 @@
 module firebird
 
-const test_url = 'firebird://fbusr:fbpwd@localhost:3050'
+const test_url = 'firebird://fbusr:fbpwd@127.0.0.1:3050/var/lib/firebird/data/firebird.fdb'
 
-fn test_new_connection() {
+fn test_new_connection() ! {
 	mut conn := open(test_url)!
 	conn.close()!
 }
