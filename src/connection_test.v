f6394491b5d227e09aa0c8edfa634df85d1706b6
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-03-08 17:40:13 +0100

    Fix typo of doom

--- src/connection_test.v
+++ src/connection_test.v
@@ -7,20 +7,15 @@ const host = '127.0.0.1:3050'
 const database = '/var/lib/firebird/data/firebird.fdb'
 const url = '${protocol}${user}:${password}@${host}${database}'
 
-// fn test_open_no_db() {
-// 	mut conn := open('${protocol}${user}@${host}') or {
-// 		assert true // protocol error: no database is provided
-// 		return
-// 	}
-// 	conn.close() or { panic(err) }
-// }
+fn test_open_no_db() {
+	mut conn := open('${protocol}${user}@${host}') or {
+		assert true // protocol error: no database is provided
+		return
+	}
+	conn.close() or { panic(err) }
+}
 
 fn test_open_() {
-	mut conn := open('${user}:${password}@${host}${database}') or {
-		panic(err)
-		// TODO BLOCKING
-		// [firebird] Your user name and password are not defined. Ask your database administrator to set up a Firebird login.
-		// Seems like an SRP issue: if giving wrong user, it fails before op_cont_auth
-	}
+	mut conn := open('${user}:${password}@${host}${database}') or { panic(err) }
 	conn.close() or { panic(err) }
 }
--- src/secure_remote_password_test.v
+++ src/secure_remote_password_test.v
@@ -1,9 +1,54 @@
 module firebird
 
 import crypto.rand
+import encoding.hex
 import math.big
 
 const srp_salt_size = 32
+
+// These values are obtained using Jaybird
+fn test_get_client_proof() {
+	user := 'FBUSR'
+	password := 'fbpwd'
+	plugin_name := 'Srp256'
+	salt := hex.decode('41434331313038393841383533333930413434394436433832324642323941344135444339343031373532333533353331443030334235363244323036384333') or {
+		panic(err)
+	}
+	client_public_key := big.integer_from_radix('b9c5052c23b826cd50390142b2bbb8a6b72809594d3e2ec9ef1de270f0fd654b4c1f6ef9354bd6a6905fad1f8ea2e1fc7d502a81da7d9bb6cf3ead2fe9e8de842ec9eab0aa008b4f50dbccc0538e8ce9f15e00c667f3f8f69e498c750f55f71f0b7ade139fa7406a95468b1257e8e54bc91248c11481196922cf84b5be74e055',
+		16) or { panic(err) }
+	server_public_key := big.integer_from_radix('6f460f8184112b6bd6e14d833e4eb881a17b25c6fb70bbe02db67528a4d4f213adf272af080574b44cda8d63ccc7f66482a73150156576e8f4998fe0dd9724d1182170f15aa6222f81e04a7abb05a1fee54209734f307f42468919565a082b12418c700db9cc1e50f88dd39cce32df0bded4f68c06ca8a851373fb432cd0fc9f',
+		16) or { panic(err) }
+	client_secret_key := big.integer_from_radix('676add8528d92dd956aab0332a197462', 16) or {
+		panic(err)
+	}
+	expected_scramble := big.integer_from_radix('34239ef7c032bd74949aac7892d68dff22a84049',
+		16) or { panic(err) }
+	expected_user_hash := big.integer_from_radix('b5397eedf64eacec1be2fb66ffd7009f2381dd5a',
+		16) or { panic(err) }
+	expected_k := hex.decode('c6837f71b3a8f342921fe19ac0d08830ef86ab0a') or { panic(err) }
+	expected_m := hex.decode('e3f01a2478148df58a5f124e5fff37b8c46c3277ff237df83e54baf6890a9899') or {
+		panic(err)
+	}
+
+	// session_key
+	session_key := get_session_key(user, password, salt, client_public_key, server_public_key,
+		client_secret_key)
+	assert expected_k == session_key
+
+	// get_scramble
+	scramble := get_scramble(client_public_key, server_public_key)
+	assert scramble == expected_scramble
+
+	// get_user_hash
+	hash := get_user_hash(salt, user, password)
+	assert hash == expected_user_hash
+
+	// get_client_proof
+	m, k := get_client_proof(user, password, salt, client_public_key, server_public_key,
+		client_secret_key, plugin_name)
+	assert k == expected_k
+	assert m == expected_m
+}
 
 fn get_verifier(user string, password string, salt []u8) big.Integer {
 	prime, g, _ := get_prime()
