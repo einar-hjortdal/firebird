f80fa34f91c70f9b6bda2d583f7041054ec23164
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-03-22 09:45:00 +0100

    Uncomment test

--- src/connection_test.v
+++ src/connection_test.v
@@ -7,13 +7,13 @@ const host = '127.0.0.1:3050'
 const database = '/var/lib/firebird/data/firebird.fdb'
 const url = '${protocol}${user}:${password}@${host}${database}'
 
-// fn test_open_no_db() {
-// 	mut conn := open('${protocol}${user}@${host}') or {
-// 		assert true // protocol error: no database is provided
-// 		return
-// 	}
-// 	conn.close() or { panic(err) }
-// }
+fn test_open_no_db() {
+	mut conn := open('${protocol}${user}@${host}') or {
+		assert true // protocol error: no database is provided
+		return
+	}
+	conn.close() or { panic(err) }
+}
 
 fn test_open_() {
 	mut conn := open('${user}:${password}@${host}${database}') or { panic(err) }
