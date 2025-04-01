129f99a770ea78b353aedd7acc45362ce28544ed
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-04-01 11:46:49 +0200

    Add test query

--- src/statement_test.v
+++ src/statement_test.v
@@ -5,8 +5,11 @@ import tests
 fn test_new_statement() {
 	mut conn := new_connection(tests.url) or { panic(err) }
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
-	mut stmt := tx.prepare_statement('CREATE TABLE test')!
+	mut stmt := tx.prepare_statement('CREATE TABLE foo (a INTEGER)')!
 	stmt.close()!
 	tx.rollback()!
 	conn.close() or { panic(err) }
+}
+
+fn test_execute_statement() {
 }
