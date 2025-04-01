7ab941ed7be10b614babfb76291d877d0614f21b
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-04-01 18:28:21 +0200

    Unexpected

--- src/statement_test.v
+++ src/statement_test.v
@@ -7,6 +7,9 @@ fn test_new_statement() {
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 	mut stmt := tx.prepare_statement('CREATE TABLE foo (a INTEGER)')!
 	stmt.close()!
+	println(stmt.tx.conn.p.lazy_response_count) // 1
+	println(tx.conn.p.lazy_response_count) // 0
+	// Despite all structs being `mut`. Why is this happening?
 	tx.rollback()!
 	conn.close()!
 }
