1bed1908f572c67551d0495235136c51f6174d23
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-04-04 11:19:16 +0200

    Add more tests

--- src/statement_test.v
+++ src/statement_test.v
@@ -4,6 +4,15 @@ import tests
 
 const no_args = []Value{}
 
+// To investigate manually:
+// sudo docker run \
+//   --rm \
+//   -it \
+//   --name=firebird-client \
+//   --network=host \
+//   firebirdsql/firebird \
+//   isql -u fbusr -p fbpwd localhost:/var/lib/firebird/data/firebird.fdb
+
 // fn test_new_statement() {
 // 	mut conn := new_connection(tests.url)!
 // 	mut tx := conn.start_transaction(isolation_level_read_commited)!
@@ -20,19 +29,19 @@ fn test_execute_statement_no_args() {
 	mut conn := new_connection(tests.url)!
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 
-	mut stmt_create := tx.prepare_statement('CREATE TABLE foo (a INTEGER)')!
-	stmt_create.execute(no_args)!
-	stmt_create.execute(no_args) or {
+	mut stmt := tx.prepare_statement('CREATE TABLE foo (a INTEGER)')!
+	stmt.execute(no_args)!
+	stmt.execute(no_args) or {
 		// [firebird] unsuccessful metadata update
 		// CREATE TABLE FOO failed
 		// Table FOO already exists
 		assert err.msg().contains('Table FOO already exists')
 	}
-	stmt_create.close()!
+	stmt.close()!
 
-	mut stmt_drop := tx.prepare_statement('DROP TABLE foo')!
-	stmt_drop.execute(no_args)!
-	stmt_drop.execute(no_args) or {
+	stmt = tx.prepare_statement('DROP TABLE foo')!
+	stmt.execute(no_args)!
+	stmt.execute(no_args) or {
 		// [firebird] unsuccessful metadata update
 		// DROP TABLE FOO failed
 		// SQL error code = -607
@@ -40,30 +49,50 @@ fn test_execute_statement_no_args() {
 		// Table FOO does not exist
 		assert err.msg().contains('Table FOO does not exist')
 	}
+	stmt.close()!
 
-	stmt_create = tx.prepare_statement("CREATE TABLE foo (
-	 	a INTEGER NOT NULL,
-	 	b VARCHAR(30) NOT NULL UNIQUE,
-	 	c VARCHAR(1024),
-	 	d DECIMAL(16,3) DEFAULT -0.123,
-	 	e DATE DEFAULT '1967-08-11',
-	 	f TIMESTAMP DEFAULT '1967-08-11 23:45:01',
-	 	g TIME DEFAULT '23:45:01',
-	 	h BLOB SUB_TYPE 1,
-	 	i DOUBLE PRECISION DEFAULT 0.0,
-	 	j FLOAT DEFAULT 0.0,
-	 	PRIMARY KEY (a),
-	 	CONSTRAINT CHECK_A CHECK (a <> 0)
-	 	)")!
-	stmt_create.execute(no_args)!
-	stmt_create.close()!
+	stmt = tx.prepare_statement("
+		CREATE TABLE foo (
+			a INTEGER NOT NULL,
+			b VARCHAR(30) NOT NULL UNIQUE,
+			c VARCHAR(1024),
+			d DECIMAL(16,3) DEFAULT -0.123,
+			e DATE DEFAULT '1967-08-11',
+			f TIMESTAMP DEFAULT '1967-08-11 23:45:01',
+			g TIME DEFAULT '23:45:01',
+			h BLOB SUB_TYPE 1,
+			i DOUBLE PRECISION DEFAULT 0.0,
+			j FLOAT DEFAULT 0.0,
+			PRIMARY KEY (a),
+			CONSTRAINT CHECK_A CHECK (a <> 0)
+			)")!
+	stmt.execute(no_args)!
+	stmt.close()!
 
-	stmt_drop.execute(no_args)!
-	stmt_drop.close()!
+	tx.commit()!
+	tx = conn.start_transaction(isolation_level_read_commited)!
 
-	tx.rollback()!
+	stmt = tx.prepare_statement("
+		INSERT INTO foo (a, b, c, h) 
+			VALUES (1, 'a', 'b', 'This is a test')")!
+	stmt.execute(no_args)!
+	stmt.execute(no_args) or {
+		// [firebird] violation of PRIMARY or UNIQUE KEY constraint "INTEG_83" on table "FOO"
+		// Problematic key value is ("B" = 'a')
+		assert err.msg().contains('violation of PRIMARY or UNIQUE KEY constraint')
+	}
+	stmt.close()!
+
+	stmt = tx.prepare_statement('DROP TABLE foo')!
+	stmt.execute(no_args)!
+	stmt.close()!
+
+	tx.commit()!
 	conn.close()!
 }
+
+// fn test_execute_statement_with_args() {
+// }
 
 // fn test_result_without_data() {
 // }
