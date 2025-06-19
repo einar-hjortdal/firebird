80c9caaf7a194542d6253e21f90eb1f8eb55d661
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-06-19 17:38:16 +0200

    Update tests

--- protocol_test.v
+++ protocol_test.v
@@ -4,7 +4,7 @@ fn test_marshal_i32_positive() {
 	i32_positive := i32(305419896)
 	expected := [u8(18), 52, 86, 120]
 
-	res := marshal_i32(i32_positive)
+	res := marshal_i32_big_endian(i32_positive)
 
 	assert res == expected
 }
@@ -13,7 +13,7 @@ fn test_marshal_i32_negative() {
 	i32_positive := i32(-305419896)
 	expected := [u8(237), 203, 169, 136]
 
-	res := marshal_i32(i32_positive)
+	res := marshal_i32_big_endian(i32_positive)
 
 	assert res == expected
 }
--- time_test.v
+++ time_test.v
@@ -4,12 +4,12 @@ import time
 
 fn test_to_blr_date() {
 	selected_date := time.parse_iso8601('2020-04-05')!
-	d, _ := new_date(selected_date).to_blr()!
+	_, d := new_date(selected_date).to_blr()!
 	assert d == [u8(0), 0, 230, 64]
 }
 
 fn test_to_blr_time() {
 	selected_time := time.parse_iso8601('2020-04-05T14:30:15')!
-	t, _ := new_time(selected_time).to_blr()!
+	_, t := new_time(selected_time).to_blr()!
 	assert t == [u8(31), 31, 96, 112]
 }
