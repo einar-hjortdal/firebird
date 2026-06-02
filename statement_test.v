11566658f6fdecef0ac7d5c3f0bb5ed1eb28e6d2
Einar Hjortdal <einar.hjortdal@outlook.com>
2026-06-02 14:45:32 +0200

    c error encountered

--- statement_test.v
+++ statement_test.v
@@ -1,31 +1,66 @@
 module firebird
 
+import os
 import rand
 import time
-import einar_hjortdal.luuid
+// import einar_hjortdal.luuid
 
-// To manually fix issues:
-// sudo docker run \
-//   --rm \
-//   -it \
-//   --name=firebird-client \
-//   --network=host \
-//   firebirdsql/firebird \
-//   isql -u fbusr -p fbpwd localhost:/var/lib/firebird/data/firebird.fdb
+const firebird_container_name = 'test_firebird_server'
+const firebird_port = '3051'
+const firebird_user = 'test_user'
+const firebird_root_password = 'test_root_password'
+const firebird_password = 'test_password'
+const firebird_database = 'test_database.fdb'
+const firebird_database_path = '/var/lib/firebird/data/${firebird_database}'
+const firebird_url = 'firebird://${firebird_user}:${firebird_password}@localhost:${firebird_port}${firebird_database_path}'
 
-const protocol = 'firebird://'
-const user = 'fbusr'
-const test_password = 'fbpwd'
-const host = '127.0.0.1:3050'
-const database = '/var/lib/firebird/data/firebird.fdb'
-const test_url = '${protocol}${user}:${test_password}@${host}${database}'
+// Remember to `sudo usermod -aG docker $USER`
+fn container_firebird_clean() {
+	result := os.execute('docker stop ${firebird_container_name}')
+	if result.exit_code != 0 {
+		if result.output.contains('No such container') {
+			return
+		}
+		eprintln(result.output)
+	}
+}
 
-// TODO cleanup functions: ensure manual intervention is never needed.
+fn container_firebird_start() ! {
+	container_firebird_clean() // kill container if already running
+	result :=
+		os.execute('docker run --rm --detach --name=${firebird_container_name} --env=FIREBIRD_ROOT_PASSWORD=${firebird_root_password} --env=FIREBIRD_USER=${firebird_user} --env=FIREBIRD_PASSWORD=${firebird_password} --env=FIREBIRD_DATABASE=${firebird_database} --env=FIREBIRD_DATABASE_DEFAULT_CHARSET=UTF8 --publish=${firebird_port}:3050 firebirdsql/firebird')
+	if result.exit_code != 0 {
+		return error(result.output)
+	}
+}
+
+fn container_is_ready() {
+	mut firebird_is_loading := true
+	for firebird_is_loading {
+		check :=
+			os.execute('echo "SELECT \'ALIVE\' FROM RDB\\\$DATABASE; quit;" | docker exec -i ${firebird_container_name} isql localhost:${firebird_database_path} -user ${firebird_user} -password ${firebird_password} -q')
+		if check.output.contains('ALIVE') {
+			firebird_is_loading = false
+		}
+		time.sleep(1 * time.second)
+	}
+	return
+}
+
+fn testsuite_begin() ! {
+	container_firebird_start()!
+	container_is_ready()
+}
+
+fn testsuite_end() ! {
+	container_firebird_clean()
+}
+
 // TODO use get_type utility functions to simplify assertions
 // TODO split DateTime params tests from date string params
 
 fn test_open_no_db() {
-	mut conn := new_connection('${protocol}${user}@${host}') or {
+	mut conn := new_connection('firebird://${firebird_user}:${firebird_password}@localhost') or {
 		assert true // protocol error: no database is provided
 		return
 	}
@@ -33,12 +68,12 @@ fn test_open_no_db() {
 }
 
 fn test_open() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 	conn.close()!
 }
 
 fn test_new_statement() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 
 	mut stmt := tx.prepare('CREATE TABLE foo (a INTEGER)')!
@@ -49,7 +84,7 @@ fn test_new_statement() {
 }
 
 fn test_execute_statement_ddl() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 
 	mut stmt := tx.prepare('CREATE TABLE foo (a INTEGER)')!
@@ -79,7 +114,7 @@ fn test_execute_statement_ddl() {
 }
 
 fn test_at_time_zone() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 
 	mut result := tx.execute('SELECT current_timestamp FROM RDB\$DATABASE')!
@@ -119,7 +154,7 @@ fn test_at_time_zone() {
 }
 
 fn test_time_zone() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 	tx.execute('CREATE TABLE foo (
 		id INTEGER PRIMARY KEY,
@@ -203,7 +238,7 @@ fn test_time_zone() {
 // }
 
 fn test_execute_dml() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 	mut stmt := tx.prepare("CREATE TABLE foo (
@@ -263,7 +298,7 @@ fn test_execute_dml() {
 }
 
 fn test_null() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 	mut stmt := tx.prepare('CREATE TABLE foo (
@@ -309,7 +344,7 @@ fn test_null() {
 }
 
 fn test_statement_params() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 	tx.execute('CREATE TABLE foo (
@@ -435,7 +470,7 @@ fn test_statement_params() {
 }
 
 fn test_statement_time_params() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 	tx.execute('CREATE TABLE foo (
@@ -455,7 +490,8 @@ fn test_statement_time_params() {
 	tx.execute('INSERT INTO foo (id, b) VALUES (?, ?)', i32(2), '12:34:56')!
 	tx.execute('INSERT INTO foo (id, c) VALUES (?, ?)', i32(3), '12:34:56 +02:00')!
 	tx.execute('INSERT INTO foo (id, d) VALUES (?, ?)', i32(4), '2025-02-12 12:34:56')!
-	tx.execute('INSERT INTO foo (id, e) VALUES (?, ?)', i32(5), '2025-02-12 12:34:56 Europe/Brussels')!
+	tx.execute('INSERT INTO foo (id, e) VALUES (?, ?)', i32(5),
+		'2025-02-12 12:34:56 Europe/Brussels')!
 
 	// same data, but as DateTime objects
 	d := new_date(time.parse_iso8601('2025-02-12')!)
@@ -574,7 +610,7 @@ fn test_statement_time_params() {
 }
 
 fn test_large_returns() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 	tx.execute('CREATE TABLE foo (
@@ -597,55 +633,8 @@ fn test_large_returns() {
 	tx.rollback()!
 }
 
-fn test_luuid() {
-	mut gen := luuid.new_generator()
-	id := gen.v1()
-	id_bin := luuid.to_bytes(id)!
-
-	mut conn := new_connection(test_url)!
-
-	mut tx := conn.start_transaction(isolation_level_read_commited)!
-	tx.execute('CREATE TABLE foo (
-		id BINARY(16) PRIMARY KEY NOT NULL,
-		a BINARY(16)
-		)')!
-	tx.commit()!
-
-	tx = conn.start_transaction(isolation_level_read_commited)!
-
-	// This query inserts id using CHAT_TO_UUID in the column id, and id as []u8 in the column a
-	tx.execute('INSERT INTO foo (id, a) VALUES (CHAR_TO_UUID(?), ?)', id, id_bin)!
-
-	// The following query will prove that firebirds stores id and a identically
-	res := tx.execute('SELECT UUID_TO_CHAR(id), a FROM foo WHERE id = a')!
-	tx.rollback()!
-
-	tx = conn.start_transaction(isolation_level_read_commited)!
-	tx.execute('DROP TABLE foo')!
-	tx.commit()!
-
-	conn.close()!
-
-	// If the value in both columns is the same, we expect one row of results.
-	assert res.rows.len == 1
-
-	// Now the row values are converted and compared at the app level
-	got_id, _ := res.rows[0].values[0].get_string()!
-	a_bin, _ := res.rows[0].values[1].get_array_u8()!
-	got_a := luuid.from_bytes(a_bin)!
-
-	// got_id contains whitespaces that needs to be trimmed.
-	// Don't know why this happens. got.id.len == 144, 4 times 36.
-	// The firebird server sends this array as text with utf8 encoding
-	// [48, 54, 56, 50, 52, 53, 53, 70, 45, 55, 50, 49, 52, 45, 49, 66, 52, 66, 45, 51, 67, 48, 48, 45, 55, 49, 68, 66, 69, 57, 69, 57, 65, 69, 48, 67, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32]
-	// All those 32 after the id make no sense to me.
-	//
-	// got_a needs to be normalized to uppercase because firebird always normalizes to uppercase.
-	assert got_id.trim_space() == got_a.to_upper()
-}
-
 fn test_char_boolean() {
-	mut conn := new_connection(test_url)!
+	mut conn := new_connection(firebird_url)!
 	mut tx := conn.start_transaction(isolation_level_read_commited)!
 	tx.execute('CREATE TABLE foo (
 		id CHAR(3) PRIMARY KEY NOT NULL,
